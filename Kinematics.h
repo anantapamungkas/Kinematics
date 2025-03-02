b4349dbf60ec50c52cb955566e619a90413f8288
Ananta Pamungkas <95330290+anantapamungkas@users.noreply.github.com>
2025-03-02 19:51:37 +0700

    Update distance error flag

--- Kinematics.h
+++ Kinematics.h
@@ -15,10 +15,16 @@ private:
   float angles[4];  // Dynamic wheel angles
 
   float calculatePID(float desiredValue, float currentValue);
+  void updateErrors();  // Private function to update distanceError & angleError
 
 public:
   float wheelA, wheelB, wheelC, wheelD;
   float currentX, currentY, currentAngle;  // Updated dynamically from sensors
+  float targetX, targetY, targetAngle;     // Store target values
+
+  // Publicly accessible error variables
+  float distanceError;
+  float angleError;
 
   Kinematics(float maxSpeed, float minSpeed = 0, float integralMax = 10.0);
 
